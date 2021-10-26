cdb7d3a8a834b36a9fe08eb6c190c387007e9629
edwgk <edwg1782@gmail.com>
2021-10-25 20:42:21 -0500

    first example app

---
+++ app/src/main/java/com/example/helloeverybody/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloeverybody;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
